fc47faf39ff15324a5267b4089acaf2849084ace
nhat doan <nhatdoanexpo@gmail.com>
2023-03-25 22:02:41 +0700

    up

--- src/debian/install/anydesk.sh
+++ src/debian/install/anydesk.sh
@@ -3,7 +3,8 @@
 set -e
 
 echo "Any desk to remote"
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add - 
-echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk.list 
-apt update && apt install -y anydesk
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+echo "deb http://deb.anydesk.com/ all main" | tee /etc/apt/sources.list.d/anydesk-stable.list
+apt update
+apt install anydesk
 apt-get clean -y
